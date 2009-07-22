12938d9f4bed22f45b4f30f32c7c2d4800414912
Philip Blyth <philip.blyth@gmail.com>
2009-07-22 17:07:34 -0400

    settings: added basics

---
+++ models/model.setting.php
@@ -0,0 +1,18 @@
+<?php
+
+class Setting extends EndoModel {
+
+  function Group($group)
+  {
+    return AppModel::FindAll('Setting', false, "`group`='{$group}'", '`variable` ASC');
+  }
+
+  function Get($variable, $group = 'default', $value_only = false)
+  {
+    $tmp = AppModel::FindFirst('Setting', false, "`variable`=\"{$variable}\" AND `group`='{$group}'");
+    return !$value_only ? $tmp : $tmp->value;
+  }
+
+}
+
+?>
