2000375329ae035c17ffaa3a16cd37e2ff4983b4
Philip Blyth <philip.blyth@gmail.com>
2009-07-22 17:03:42 -0400

    view class: better 'assign'

--- classes/class.endo_view.php
+++ classes/class.endo_view.php
@@ -8,40 +8,46 @@
 class EndoView extends Smarty
 {
 
-	function __construct()
-	{
-		$this->template_dir = SMARTY_TEMPLATE_DIR;
-		$this->compile_dir = SMARTY_COMPILE_DIR;
-		$this->config_dir = SMARTY_CONFIG_DIR;
-		$this->cache_dir = SMARTY_CACHE_DIR;
-		$this->plugins_dir = array_merge(
-		  array(
-		    APP_ROOT.SMARTY_TEMPLATE_DIR.DS.SMARTY_PLUGINS_DIR,
-		    ENDO_ROOT.SMARTY_TEMPLATE_DIR.DS.SMARTY_PLUGINS_DIR
-		  ),
-		  $this->plugins_dir
+  function __construct()
+  {
+    $this->template_dir = SMARTY_TEMPLATE_DIR;
+    $this->compile_dir = SMARTY_COMPILE_DIR;
+    $this->config_dir = SMARTY_CONFIG_DIR;
+    $this->cache_dir = SMARTY_CACHE_DIR;
+    $this->plugins_dir = array_merge(
+      array(
+        APP_ROOT.SMARTY_TEMPLATE_DIR.DS.SMARTY_PLUGINS_DIR,
+        ENDO_ROOT.SMARTY_TEMPLATE_DIR.DS.SMARTY_PLUGINS_DIR
+      ),
+      $this->plugins_dir
     );
 
-		$this->caching = SMARTY_CACHING;
-		$this->compile_check = SMARTY_COMPILE_CHECK;
-		$this->use_sub_dirs = true;
+    $this->caching = SMARTY_CACHING;
+    $this->compile_check = SMARTY_COMPILE_CHECK;
+    $this->use_sub_dirs = true;
 
     $this->default_template_handler_func = 'template_handler';
 
-		$this->debugging = DEBUG==2 ? true : false;
-		$this->error_reporting = DEBUG>=1 ? true : false;
+    $this->debugging = DEBUG==2 ? true : false;
+    $this->error_reporting = DEBUG>=1 ? true : false;
 
     $this->assign('_smarty_debug_output', SMARTY_DEBUG_OUTPUT);
 
     $this->autoload_filters = array(
       'pre' => array('fix_literal')
     );
-	}
+  }
 
-	function assign_from($other_view)
-	{
-    $this->assign($other_view->_tpl_vars);
-	}
+  function assign_from($other_view)
+  {
+    return $this->assign($other_view->_tpl_vars);
+  }
+
+  function assign($tpl_var, $value = null)
+  {
+    parent::assign($tpl_var, $value);
+    return is_array($tpl_var) ? $tpl_var : $value;
+  }
 
 }
 
