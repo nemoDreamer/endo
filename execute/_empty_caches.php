62d7ad4bff6bb1560216e0f019d0779d3c5a9c3c
Philip Blyth <philip.blyth@gmail.com>
2010-02-27 22:22:28 -0500

    scripts: added removal of stamped lesson PDFs to '_empty_caches.php'

--- execute/_empty_caches.php
+++ execute/_empty_caches.php
@@ -2,6 +2,7 @@
 
 emptyResource(SMARTY_COMPILE_DIR);
 emptyResource(SMARTY_CACHE_DIR);
+emptyResource(WEB_ROOT.'uploads/lessons/stamped'); // !!!
 createEmptyFile(APP_ROOT.CACHES_DIR, 'find');
 
 // --------------------------------------------------
